5bc4a92f491959ed4b9cb7a78d70ad2541046c37
S3R6104BR3U <sergioabreusousa@gmail.com>
2024-12-15 18:06:53 -0300

    add check theme

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,31 @@
 const toggleTheme = document.getElementById("toggleTheme")
 const rootHtml = document.documentElement
+const theme = localStorage.getItem('theme')
+
+function checkTheme() {
+    const theme = localStorage.getItem('theme')
+    if (theme !== null) {
+        rootHtml.setAttribute('data-theme', theme)
+    }
+}
+
+checkTheme()
+
+function saveTheme(theme) {
+    localStorage.setItem('theme', theme)
+}
 
 function changeTheme() {
     const currentTheme = rootHtml.getAttribute("data-theme")
 
-    if(currentTheme === "dark") rootHtml.setAttribute("data-theme", "light")
-    else rootHtml.setAttribute("data-theme", "dark")
+    if(currentTheme === "dark") {
+        rootHtml.setAttribute("data-theme", "light")
+        saveTheme('light')
+    }
+    else {
+        rootHtml.setAttribute("data-theme", "dark")
+        saveTheme('dark')
+    }
 
     toggleTheme.classList.toggle("bi-sun")
     toggleTheme.classList.toggle("bi-moon-stars")
@@ -24,3 +44,13 @@ accordionHeaders.forEach(headers => {
         accordionActive ? accordionItem.classList.remove('active') : accordionItem.classList.add('active')
     })
 })
+
+const menuLinks = document.querySelectorAll('.menu__link')
+
+menuLinks.forEach(item => {
+    item.addEventListener('click', () => {
+        menuLinks.forEach(i => i.classList.remove('active'))
+        item.classList.add('active')
+    })
+})
+
